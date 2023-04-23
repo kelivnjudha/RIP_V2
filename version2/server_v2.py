869a38a3021357d378613f52a7eb8aa5c2c5bfbc
Kelvin <kelvin.7o2.k3@gmail.com>
2023-04-24 01:24:16 +0630

    Update server code/ fix some error.

--- version2/server_v2.py
+++ version2/server_v2.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 ADMIN_DATA = {
-    "Admin1": "200210",
+    "Admin": "200210",
     "Admin2": "admin_password2"
 }
 
@@ -15,9 +15,12 @@ server.bind((HOST, PORT))
 clients = []
 client_ips = []
 nicknames = []
+client_device_ids = []
 
-def broadcast(message):
+def broadcast(message, to_admins_only=False):
     for client in clients:
+        if to_admins_only and not nicknames[clients.index(client)].startswith("Admin-"):
+            continue
         client.send(message)
 
 def handle_connection(client, addr):
@@ -28,20 +31,39 @@ def handle_connection(client, addr):
     while not stop:
         try:
             message = client.recv(1024).decode("utf-8")
-            target_ip, _, message_content = message.partition(": ")
+            if not message:
+                break
+            print(f"Message received: {message}")
+            nickname = nicknames[clients.index(client)]
+            if nickname.startswith("Admin-"):
+                _, _, message_content = message.partition(": ")
+                target_nickname, _, message_content = message.partition(": ")
 
-            if client in clients and nicknames[clients.index(client)].startswith("Admin-"):
-                send_message_to_target(target_ip, message_content)
+                if message.startswith("-usr_info"):
+                    client.send(active_users_info().encode("utf-8"))
+
+                else:
+                    print(f"Forwarding message to target: {target_nickname}")
+                    send_message_to_target(target_nickname, message_content)
+                    print(f"Message forwarded to target: {target_nickname}")
             else:
-                broadcast(message.encode("utf-8"))
+                admin_nickname, _, result_message = message.partition(": ")
+                admin_nickname = admin_nickname.strip()
+                if admin_nickname.startswith("Admin-"):
+                    admin_nickname = f"Admin-{client_ip}"
+                    send_message_to_target(admin_nickname,f"{nickname}: {result_message}")
+                else:
+                    broadcast(message.encode("utf-8"))
 
-        except:
+        except Exception as e:
+            print(f"Error in handle connection: {e}")
             index = clients.index(client)
             clients.remove(client)
             client_ips.remove(client_ip)
             nickname = nicknames[index]
             nicknames.remove(nickname)
-            broadcast(f"{nickname} Disconnected".encode("utf-8"))
+            if not nickname.startswith("Admin-"):
+                broadcast(f"{nickname} Disconnected".encode("utf-8"))
             stop = True
 
 def active_users_info():
@@ -49,13 +71,22 @@ def active_users_info():
     user_list = ", ".join(nicknames)
     return f"Number of active users: {user_count}\nNicknames: {user_list}"
 
-def send_message_to_target(target_ip, message):
-    if target_ip in client_ips:
-        index = client_ips.index(target_ip)
-        target_client = clients[index]
-        target_client.send(f"Admin: {message}".encode("utf-8"))
-    else:
-        print(f"User with IP '{target_ip}' not found.")
+def send_message_to_target(target_nickname, message):
+    target_found = False
+    for index, nickname in enumerate(nicknames):
+        if nickname == target_nickname:
+            target_found = True
+            target_client = clients[index]
+            target_client.send(f"Admin: {message}".encode("utf-8"))
+            print(f"Message sent to target: {target_nickname}")
+            break
+    if not target_found:
+        admin_index = [i for i, nickname in enumerate(nicknames) if nickname.startswith("Admin-") ]
+        if admin_index:
+            admin_client = clients[admin_index[0]]
+            admin_client.send(f"User '{target_nickname}' not found".encode("utf-8"))
+        else:
+            print(f"User with IP '{target_nickname}' not found.")
 
 def main():
     server.listen()
@@ -64,7 +95,7 @@ def main():
         client, addr = server.accept()
         ip, _ = addr
 
-        client.send("ENTER USERNAME:".encode('utf-8'))
+        client.send("ENTER NICKNAME:".encode('utf-8'))
         username = client.recv(1024).decode("utf-8")
 
         if username in ADMIN_DATA:
@@ -86,14 +117,16 @@ def main():
                 client.close()
                 continue
         else:
+            client.send("ENTER DEVICE ID:".encode("utf-8"))
+            device_id = client.recv(1024).decode("utf-8")
             print(f"Connected to {addr}")
-            client.send("ENTER DEVICE NAME:".encode('utf-8'))
-            device_name = client.recv(1024).decode("utf-8")
-            nickname = f"{username}-{device_name}"
+            nickname = f"{username}-{device_id}"
             nicknames.append(nickname)
+            client_ips.append(ip)
+            client_device_ids.append(device_id)
             clients.append(client)
             print(f"Nickname is {nickname}")
-            broadcast(f"{nickname} Connected".encode('utf-8'))
+            broadcast(f"{nickname} Connected".encode('utf-8'), to_admins_only=True)
             client.send("You are now connected!".encode('utf-8'))
             thread = threading.Thread(target=handle_connection, args=(client, addr))
             thread.start()
